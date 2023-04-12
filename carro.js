cc5b768fd3d05f499f8bb0e1fc0a8e27947ed75b
Fábio Alarcon de Almeida <124197923+Roolligan@users.noreply.github.com>
2023-04-12 19:01:22 -0300

    Update carro.js

--- carro.js
+++ carro.js
@@ -1,39 +1,30 @@
 //variaveis do carro
-// carro 1
-let xCarro = 600;
-let yCarro = 40;
-let velocidadeCarro1 = 2;
+// código dos carros
 
-// carro 2
-let xCarro2 = 600;
-let yCarro2 = 100;
-let velocidadeCarro2 = 3.2;
-
-// carro 3
-let xCarro3 = 600;
-let yCarro3 = 150;
-let velocidadeCarro3 = 2.5;
+let xCarros =[600, 600, 600];
+let yCarros = [40, 96, 150];
+let velocidadeCarros = [2, 3.2, 2.5]
 
 function mostraCarro(){
-  image(imagemCarro, xCarro, yCarro, 65, 40);
-  image(imagemCarro2, xCarro2, yCarro2, 65, 40);
-  image(imagemCarro3, xCarro3, yCarro3, 65, 40);
+  image(imagemCarro, xCarros[0], yCarros[0], 65, 40);
+  image(imagemCarro2, xCarros[1], yCarros[1], 65, 40);
+  image(imagemCarro3, xCarros[2], yCarros[2], 65, 40);
 }
 
 function movimentaCarro(){
-  xCarro -= velocidadeCarro1;
-  xCarro2 -= velocidadeCarro2;
-  xCarro3 -= velocidadeCarro3;
+  xCarros[0] -= velocidadeCarros[0];
+  xCarros[1] -= velocidadeCarros[1];
+  xCarros[2] -= velocidadeCarros[2];
 }
 
 function voltaCarro(){
-  if (xCarro < -60){
-    xCarro = 600
+  if (xCarros[0] < -60){
+    xCarros[0] = 600
   }
-  if (xCarro2 < -60){
-    xCarro2 = 600
+  if (xCarros[1] < -60){
+    xCarros[1]= 600
   }
-  if (xCarro3 < -60){
-    xCarro3 = 600
+  if (xCarros[2] < -60){
+    xCarros[2] = 600
   } 
 }
